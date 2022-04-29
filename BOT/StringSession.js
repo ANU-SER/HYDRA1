42175d5028956c5537ebb4c9f1e94a35d1b3d027
ANSHID <93968310+ANU-SER@users.noreply.github.com>
2022-04-29 09:38:58 +0530

    Update StringSession.js

--- BOT/StringSession.js
+++ BOT/StringSession.js
@@ -13,7 +13,7 @@ class StringSession {
     }
 
     deCrypt(string = undefined) {
-        if ('ANU-SER_SESSION' in process.env && string === undefined) {
+        if ('ANUSER_SESSION' in process.env && string === undefined) {
             string = process.env.STRING_SESSION;
         } else if (string !== undefined) {
             if (fs.existsSync(string)) {
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'ANU-SER;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'ANUSER;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
