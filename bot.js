8d2261e5b49bf68fe7f2b8ad377efdf8803bfead
ANSHID <93968310+ANU-SER@users.noreply.github.com>
2022-04-29 11:11:55 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -140,7 +140,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
         });
 
         console.log(
-            chalk.green.bold('✅️RUDHRA working ' + config.WORKTYPE + ' 𝚗𝚘𝚠 '));
+            chalk.green.bold('✅️HYDRA working ' + config.WORKTYPE + ' 𝚗𝚘𝚠 '));
           // thanks to afnanplk
 	    if (config.LANG == 'EN' || config.LANG == 'ML') {
                 await git.fetch();
@@ -339,8 +339,8 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
                         } catch (error) {
                             if (config.NOLOG == 'off') {
                                 
-                                await conn.sendMessage(conn.user.jid, '*~_________~ ༄ƦUÐҤƦλ࿐ ~______~*' +
-                                    '\n\n*😵 ' + error + '*\n\n```Report errors\njoin ⚠️Warning bot not allowed in the group\nchat.whatsapp.com/HVpTaTICeUi2G7hPlUlGUP``` ' 
+                                await conn.sendMessage(conn.user.jid, '*~_________~ ༄卄ሃ🇩 ℝᵃ࿐ ~______~*' +
+                                    '\n\n*😵 ' + error + '*\n\n```Report errors\njoin ⚠️Warning bot not allowed in the group  https://chat.whatsapp.com/J30gmD5uXyu3Bw8narhkkz``` ' 
                                     , MessageType.text);
                             }
                         }
